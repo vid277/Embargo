453f482a4767691b1c8625d248e82bbf55b65688
Albert Han <albertisgood123@gmail.com>
2022-02-21 19:30:29 -0600

    Added Comments

--- Unity_Scripts/Current/Game/CameraMove.cs
+++ Unity_Scripts/Current/Game/CameraMove.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Moves the camera to follow the player
 public class CameraMove : MonoBehaviour
 {
     public Transform target;
